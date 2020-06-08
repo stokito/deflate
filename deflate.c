b8e22d05503ea704a529be5c684534722ef62379
Sergey Ponomarev <stokito@gmail.com>
2020-06-08 22:29:52 +0300

    add inflate

--- deflate.c
+++ deflate.c
@@ -84,6 +84,72 @@ int def(FILE *source, FILE *dest, int level, int mem_level, bool ended) {
     return ret;
 }
 
+#ifdef DECOMPRESS
+/* Decompress from file source to file dest until stream ends or EOF.
+   inf() returns Z_OK on success, Z_MEM_ERROR if memory could not be
+   allocated for processing, Z_DATA_ERROR if the deflate data is
+   invalid or incomplete, Z_VERSION_ERROR if the version of zlib.h and
+   the version of the library linked do not match, or Z_ERRNO if there
+   is an error reading or writing the files. */
+int inf(FILE *source, FILE *dest)
+{
+    int ret;
+    unsigned have;
+    z_stream strm;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
+
+    /* allocate inflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    strm.avail_in = 0;
+    strm.next_in = Z_NULL;
+//    ret = inflateInit(&strm);
+    ret = inflateInit2(&strm, WINDOW_BITS_RAW_DEFLATE);
+    if (ret != Z_OK)
+        return ret;
+
+    /* decompress until deflate stream ends or end of file */
+    do {
+        strm.avail_in = fread(in, 1, CHUNK, source);
+        if (ferror(source)) {
+            (void)inflateEnd(&strm);
+            return Z_ERRNO;
+        }
+        if (strm.avail_in == 0)
+            break;
+        strm.next_in = in;
+
+        /* run inflate() on input until output buffer not full */
+        do {
+            strm.avail_out = CHUNK;
+            strm.next_out = out;
+            ret = inflate(&strm, Z_NO_FLUSH);
+            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+            switch (ret) {
+                case Z_NEED_DICT:
+                    ret = Z_DATA_ERROR;     /* and fall through */
+                case Z_DATA_ERROR:
+                case Z_MEM_ERROR:
+                    (void)inflateEnd(&strm);
+                    return ret;
+            }
+            have = CHUNK - strm.avail_out;
+            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+                (void)inflateEnd(&strm);
+                return Z_ERRNO;
+            }
+        } while (strm.avail_out == 0);
+
+        /* done when inflate() says it's done */
+    } while (ret != Z_STREAM_END);
+
+    /* clean up and return */
+    (void)inflateEnd(&strm);
+    return ret;
+}
+#endif
 
 /* report a zlib or i/o error */
 void zerr(int ret) {
@@ -273,8 +339,8 @@ int main(int argc, char **argv) {
 #ifdef NO_HELP
         while ((opt = getopt(argc, argv, SHORT_OPTS)) != -1) {
 #else
-        int longopt_idx = 0;
-        while ((opt = getopt_long(argc, argv, SHORT_OPTS, longopts, &longopt_idx)) != -1) {
+    int longopt_idx = 0;
+    while ((opt = getopt_long(argc, argv, SHORT_OPTS, longopts, &longopt_idx)) != -1) {
 #endif
         switch (opt) {
             case 'h':
@@ -403,7 +469,14 @@ int main(int argc, char **argv) {
     }
 
     FILE *in_file = input_file_name ? fopen(input_file_name, "r") : stdin;
-    ret = def(in_file, out_file, global_args.compression_level, global_args.mem_level, global_args.ended);
+    /* do decompression if -d specified */
+    if (global_args.decompress) {
+        #ifdef DECOMPRESS
+        ret = inf(in_file, out_file);
+        #endif
+    } else {
+        ret = def(in_file, out_file, global_args.compression_level, global_args.mem_level, global_args.ended);
+    }
     fclose(in_file);
     fclose(out_file);
     if (ret != Z_OK)
